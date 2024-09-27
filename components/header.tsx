2f25e6f65df48ddbd828da123876e9da28ae6d41
“Techanzy <techanzy@gmail.com>
2024-09-27 13:05:15 +0500

    updates

--- components/header.tsx
+++ components/header.tsx
@@ -44,10 +44,14 @@ const Header = () => {
     router.push("/settings");
   };
 
+  const goToHome = () => {
+    router.push("/");
+  };
+
   return (
     <main>
       <div className="flex justify-between items-center py-5 px-5">
-        <div>
+        <div className="cursor-pointer" onClick={goToHome}>
           <img src="./images/logo.png" alt="logo" />
         </div>
         <div className="relative" ref={dropdownRef}>
